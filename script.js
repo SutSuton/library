7c923f31e0680b66e3f0e56cf2a57c978bfc94e6
sutsuton <sutsuton@gmail.com>
2021-10-06 20:25:45 +1100

    Added a few new functions:
    createCard(); which makes a grid which is appended within the container, and creates a grid within it containing the book's information
    
    displayBooks(); which loops through the array and calls the createCard function to display all of the books which are currently in the myLibrary array.

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return title + " by " + author + ", " + pages + ", " + read
-  }
+  // this.info = function () {
+  //   return title + " by " + author + ", " + pages + ", " + read
+        // Seems unneccessary to the current task to include this function.
+  // }
 }
 
 let userInput; // placeholder for now until I figure out how the user will interact with the website
@@ -17,14 +18,53 @@ function addBookToLibrary() {
 
 const container = document.querySelector(".container");
 
-function createCard() {
+function createCard(obj) {
   const card = document.createElement("div");
   card.className = "card";
   container.appendChild(card);
+
+  // Create the elements which will make up each card. Not sure if there is an easier way to do this.
+  const title = document.createElement("div");
+  title.className = "title";
+  card.appendChild(title);
+  title.textContent = obj.title;
+
+  const author = document.createElement("div");
+  author.className = "author";
+  card.appendChild(author);
+  author.textContent = obj.author;
+
+  const pages = document.createElement("div");
+  pages.className = "pages";
+  card.appendChild(pages);
+  pages.textContent = obj.pages;
+
+  const read = document.createElement("div");
+  read.className = "read";
+  card.appendChild(read);
+  read.textContent = obj.read;
 }
 
+// populating the array with placeholder books
+let potter = new Book("Harry Potter", "JK Rowling", 500, "read");
+userInput = potter;
+addBookToLibrary();
+
+let rings = new Book("Lord of the Rings", "JRR Tolkien", 1000, "unread");
+userInput = rings;
+addBookToLibrary();
+
+let chimes = new Book("Chimes", "Anna Smaill", 200, "unread");
+userInput = chimes;
+addBookToLibrary();
+
+let catcher = new Book("The Catcher in the Rye", "JD Salinger", 300, "read");
+userInput = catcher;
+addBookToLibrary();
 
 function displayBooks() {
-
+  for (let i = 0; i < myLibrary.length; i++) {
+    createCard(myLibrary[i]);
+  }
 }
 
